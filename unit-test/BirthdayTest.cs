6a02a72d1def5a41ddb9fcb539ad2030b184d1ac
unknown <joseph@odd-e.com>
2024-03-29 19:25:16 +0800

    :sparkles: make birthday is_not_birthday pass by manual stub

--- unit-test/BirthdayTest.cs
+++ unit-test/BirthdayTest.cs
@@ -12,17 +12,39 @@ namespace unit_test
 
         public class StubToday : Today
         {
+            private string dateStr;
+
+            public void SetToday(string dateStr)
+            {
+                this.dateStr = dateStr;
+            }
+
             override public DateTime GetToday()
             {
-                return DateTime.Parse("2002-04-09");
+                return DateTime.Parse(this.dateStr);
             }
         }
 
         [Test]
         public void is_birthday()
         {
-            Birthday birthday = new Birthday(new StubToday());
+            StubToday stubToday = new StubToday();
+            stubToday.SetToday("2002-04-09");
+
+            Birthday birthday = new Birthday(stubToday);
+
             Assert.IsTrue(birthday.IsBirthday());
+        }
+
+        [Test]
+        public void is_not_birthday()
+        {
+            StubToday stubToday = new StubToday();
+            stubToday.SetToday("2002-05-20");
+
+            Birthday birthday = new Birthday(stubToday);
+
+            Assert.IsFalse(birthday.IsBirthday());
         }
     }
 }
